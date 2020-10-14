e6fc75f22ff355c4733bd9dae3b5bc4e0a18cb96
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2020-10-14 15:27:57 -0400

    Add cors image route

---
+++ pages/api/cors-image.js
@@ -0,0 +1,25 @@
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
+import path from "path";
+import fs from "fs";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res);
+
+  // Rest of the API logic
+  const publicDir = path.resolve(process.cwd(), "./public");
+  const filePath = path.join(publicDir, "files/test.png");
+  var readStream = fs.createReadStream(filePath);
+  readStream.pipe(res);
+  //   res.json({ message: "Hello Everyone!" });
+}
